bf9e35dd076337afc142f4dddcdcbb26c97fe96d
Jenaro Garcia <jjegarcia@hotmail.com>
2022-03-11 17:50:35 +0000

    added 2 greeting components

--- app/src/main/java/com/example/telematics_engine/MainActivity.kt
+++ app/src/main/java/com/example/telematics_engine/MainActivity.kt
@@ -3,38 +3,63 @@ package com.example.telematics_engine
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.telematics_engine.ui.theme.Telematics_engineTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Telematics_engineTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp() {
+                DisplayScreenContent()
+//                Greeting(name = "Telematics")
             }
         }
     }
 }
 
 @Composable
+private fun MyApp(content: @Composable () -> Unit) {
+    Telematics_engineTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+            content()
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier.padding(16.dp)
+    )
+}
+
+@Composable
+fun DisplayScreenContent() {
+    Column {
+        Greeting(name = "Android")
+        Divider()
+        Greeting(name = "Telematics")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Telematics_engineTheme {
-        Greeting("Android")
+    MyApp {
+//        DisplayScreenContent()
+        Greeting(name = "Telematics")
     }
 }
