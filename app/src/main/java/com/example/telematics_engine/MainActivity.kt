efd0efa14649a8e2f626aef9c4f16bc279df4fd6
Jenaro Garcia <jjegarcia@hotmail.com>
2022-03-11 18:41:00 +0000

    injecting value into composed button

--- app/src/main/java/com/example/telematics_engine/MainActivity.kt
+++ app/src/main/java/com/example/telematics_engine/MainActivity.kt
@@ -6,11 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,6 +49,19 @@ fun DisplayScreenContent() {
         Greeting(name = "Android")
         Divider()
         Greeting(name = "Telematics")
+        counter()
+    }
+}
+
+@Composable
+fun counter() {
+    var count by remember {
+        mutableStateOf(0)
+    }
+    Button(onClick = {
+        count++
+    }) {
+        Text(text = "clicks: $count")
     }
 }
 
