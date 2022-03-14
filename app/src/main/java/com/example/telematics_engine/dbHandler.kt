c54055c2d9c54b343a5f6cfebef8ef1b175c19f6
Jenaro Garcia <jjegarcia@hotmail.com>
2022-03-14 07:30:01 +0000

    tidy up

--- app/src/main/java/com/example/telematics_engine/dbHandler.kt
+++ app/src/main/java/com/example/telematics_engine/dbHandler.kt
@@ -4,17 +4,13 @@ import android.util.Log
 import com.google.firebase.database.*
 
 class DbHandler() {
-    //Set up reference to DB
-    val myRef: DatabaseReference by lazy {   FirebaseDatabase.getInstance().getReference("test")}
+    val myRef: DatabaseReference by lazy { FirebaseDatabase.getInstance().getReference("test") }
 
 
     fun read(path: String) {
-        // Read from the database using reference to field above
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                Log.w("DB read- $path",snapshot.child(path).getValue().toString())
+                 Log.w("DB read- $path", snapshot.child(path).getValue().toString())
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -22,7 +18,8 @@ class DbHandler() {
             }
         })
     }
-     fun write(path: String, value: Any?){
+
+    fun write(path: String, value: Any?) {
         myRef.child(path).setValue(value)
     }
 }
